a25d83df97eb8e02853ef80b1149e0da75fc8a1d
SeoHaeYun <SeoHaeYun@github.com>
2024-04-22 15:26:03 +0900

    ad:dataList작성완료

--- app/src/main/java/kr/camp/contact/ContactListFragment.kt
+++ app/src/main/java/kr/camp/contact/ContactListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+
 import kr.camp.contact.databinding.FragmentContactlistBinding
 
 class ContactListFragment : Fragment() {
@@ -16,6 +17,4 @@ class ContactListFragment : Fragment() {
         return binding.root
     }
 }
-
-
 
