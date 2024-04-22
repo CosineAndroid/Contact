f24e12d892391bef6be08b2eeba0c1afd5ceae99
SeoHaeYun <SeoHaeYun@github.com>
2024-04-22 16:21:46 +0900

    add:리싸이클러뷰 제작 중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
