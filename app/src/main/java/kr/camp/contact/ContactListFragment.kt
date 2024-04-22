f8e252264dcddf3eb9ca6a057942c27787c8f7be
SeoHaeYun <SeoHaeYun@github.com>
2024-04-22 13:12:06 +0900

    fix:binding 지연초기화 부분 수정

--- app/src/main/java/kr/camp/contact/ContactListFragment.kt
+++ app/src/main/java/kr/camp/contact/ContactListFragment.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import kr.camp.contact.databinding.ActivityMainBinding
 import kr.camp.contact.databinding.FragmentContactlistBinding
 
 class ContactListFragment : Fragment() {
 
-    private lateinit var binding: FragmentContactlistBinding
+    private val binding by lazy { FragmentContactlistBinding.inflate(layoutInflater) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentListContact.inflate(inflater, container,false)
         return binding.root
     }
 }
