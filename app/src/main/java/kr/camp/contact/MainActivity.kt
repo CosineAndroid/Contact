debe2f584744d511b0c675fc5d8960a61233582b
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-04-22 16:21:11 +0900

    feat: ContactDetailFragment 레이아웃 및 기본 클래스 틀 추가

--- app/src/main/java/kr/camp/contact/MainActivity.kt
+++ app/src/main/java/kr/camp/contact/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.camp.contact
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kr.camp.contact.data.Contact
 import kr.camp.contact.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val fragment = ContactDetailFragment.newInstance(Contact(0, "", "", "", ""))
+        supportFragmentManager.beginTransaction().replace(R.id.testLayout, fragment).commit()
     }
 }
