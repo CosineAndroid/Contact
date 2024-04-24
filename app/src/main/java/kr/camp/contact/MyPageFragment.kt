fa0d5187ab909120e103b7bfc43e09d49a6d5589
wndnjs00 <juwonjeon852@gmail.com>
2024-04-24 13:04:32 +0900

    refactor: import 안된부분 추가

--- app/src/main/java/kr/camp/contact/MyPageFragment.kt
+++ app/src/main/java/kr/camp/contact/MyPageFragment.kt
@@ -1,5 +1,6 @@
 package kr.camp.contact
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -34,7 +35,7 @@ class MyPageFragment : Fragment() {
                     mobile: String,
                     homepage: String,
                     memo: String,
-                    image: Drawable
+                    image : Drawable
                 ) {
                     with(binding) {
                         nameTextView.text = name
